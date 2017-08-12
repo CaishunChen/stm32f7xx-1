7447a971f21dd0e57fefa77617b56765f68443b8
guangqiang.tong <tongguangqiang@aumiwalker.com>
2017-08-12 09:37:35 +0800

    工程建立，忽略SI工程，添加了sdram，qspi驱动程序，spi2中断服务

---
+++ MDK-ARM/RTE/_stm32f765_sub_board_beta/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32f765_sub_board_beta' 
+ * Target:  'stm32f765_sub_board_beta' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f7xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
